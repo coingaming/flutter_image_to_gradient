7ee1d367b0d38af4dd8e9868472496290872fba2
Maria Filippova <filippo.mariaa@gmail.com>
2025-03-25 13:29:49 +0200

    Update branch

--- dominant_color_extractor/lib/dominant_color_extractor.dart
+++ dominant_color_extractor/lib/dominant_color_extractor.dart
@@ -1,6 +1,7 @@
 library dominant_color_extractor;
 
-export 'services/dominant_color_extractor.dart';
+export 'implementations/image_color_processor_impl.dart';
 
-export 'interfaces/dominant_color_extractor_interface.dart';
+export 'interfaces/image_color_processor_interface.dart';
 export 'interfaces/image_source_manager_interface.dart';
+export 'interfaces/image_selector_interface.dart';
